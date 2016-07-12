0efea4de8f345705ab2a8d16eb9eeb0d7e4a2387
Brian Hu <billy170916@gmail.com>
2016-07-12 16:54:22 +0800

    more sections

--- Movies/MoviesTableViewController.swift
+++ Movies/MoviesTableViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import Firebase
 
 class MoviesTableViewController: UITableViewController {
-
+    
+    var ref: FIRDatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,9 +20,17 @@ class MoviesTableViewController: UITableViewController {
         self.tableView.registerNib(nib, forCellReuseIdentifier: "My Movie Cell")
         
         tableView.estimatedRowHeight = 80
-        tableView.rowHeight = UITableViewAutomaticDimension
     }
-
+    
+    func configureDatabase() {
+        ref = FIRDatabase.database().reference()
+        // Listen for new messages in the Firebase database
+        _refHandle = self.ref.child("messages").observeEventType(.ChildAdded, withBlock: { (snapshot) -> Void in
+            self.messages.append(snapshot)
+            self.clientTable.insertRowsAtIndexPaths([NSIndexPath(forRow: self.messages.count-1, inSection: 0)], withRowAnimation: .Automatic)
+        })
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -31,28 +42,64 @@ class MoviesTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
+        return 2
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 30
+        return Movie.latestMovies.count
     }
 
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        switch indexPath.section {
+        case 1:
+            return UITableViewAutomaticDimension
+        default:
+            return 60
+        }
+    }
+    
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let header = UIView()
+        
+        let screenRect = UIScreen.mainScreen().bounds
+        let screenWidth = screenRect.width
+
+        let label = UILabel(frame: CGRectMake(0, 0, screenWidth, 35))
+        header.backgroundColor = UIColor.lightGrayColor()
+        
+        if section == 0 {
+            label.text = "Standard Cells"
+        } else if section == 1 {
+            label.text = "Custom Cells"
+        }
+
+        header.addSubview(label)
+        return header
+    }
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCellWithIdentifier("My Movie Cell", forIndexPath: indexPath) as! MyMovieTableViewCell
-        let movie = Movie.latestMovies[indexPath.row%3]
-
-        cell.movieImageView.image = movie.image
-        cell.nameLabel.text = movie.name
-        cell.descriptionLabel.text = movie.description
+        let movie = Movie.latestMovies[indexPath.row]
         
-//        cell.textLabel?.text = movie.name
-//        cell.textLabel?.numberOfLines = 0
-//        cell.imageView?.image = movie.image
-
-        return cell
+        switch indexPath.section {
+        case 0:
+            let cell = tableView.dequeueReusableCellWithIdentifier("Movie Cell", forIndexPath: indexPath)
+            
+            cell.textLabel?.text = movie.name
+            cell.textLabel?.numberOfLines = 0
+            cell.imageView?.image = movie.image
+            return cell
+        case 1:
+            let cell = tableView.dequeueReusableCellWithIdentifier("My Movie Cell", forIndexPath: indexPath) as! MyMovieTableViewCell
+            
+            cell.movieImageView.image = movie.image
+            cell.nameLabel.text = movie.name
+            cell.descriptionLabel.text = movie.description
+            return cell
+        default:
+            return UITableViewCell()
+        }
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
